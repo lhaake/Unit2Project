67c8c2272d7027776ee78ed42fa48dc6fff3fa7d
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-10-09 10:23:19 -0400

    updated files

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
 
     return (
         <div className="about-page"> 
-            <p>The Daily News App was built by Leah Haake using React for the General Assembly Software Engineering Immersive Unit 2 Project using React.</p>
+            <p>The Daily News App was built by Leah Haake using React for General Assembly's Software Engineering Immersive Unit 2 Project.</p>
             <p>The API utilized for the project is <a href="https://newsapi.org/">NewsAPI</a></p>
             <p>Connect with me on <a href="https://www.linkedin.com/in/leah-haake/">LinkedIn!</a></p>
         </div>
--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,32 +1,9 @@
-import React, {useState} from "react"
+import React from "react"
 import './Article.css';
-import Favorites from "../Favorites/Favorites"
 
 const Article = (props) => {
 
-    const [favorites, setFavorites] = useState([])
-  
-
     console.log("testing props from Article", props.newsData.articles)
-
- 
-
-    const handleFavoritesClick = favarticle => {
-        console.log("Add favorites button clicked!")
-        setFavorites([...favorites, {
-            title: favarticle.title,
-            source: favarticle.source.name,
-            description: favarticle.description,
-            url: favarticle.url,
-            urlToImage: favarticle.urlToImage
-        }])
-    }
-
-    const removeFromFaves = index => {
-        console.log("Remove from favorites button clicked!")
-        const favesArray = favorites.filter((article, i) => i !== index)
-        setFavorites(favesArray)
-    }
 
     const data = props.newsData.articles
 
@@ -35,9 +12,7 @@ const Article = (props) => {
             <div className="article">
                 { article.urlToImage ?
                     <img src={article.urlToImage} /> : ""}
-                {/* { article.title !== article.title ?
-                    <h3 className="title">Title: {article.title}</h3>: null }  */}
-                {/* if the article.title is equal to a title in the array that has already been rendered, dont render (handling duplicate articles). data.includes(article.title) */}
+            
                 <h3 className="title">{article.title}</h3>
                 <ul>
                     <li className="news-source">{article.source.name}</li>
@@ -45,18 +20,14 @@ const Article = (props) => {
                         "_blank">Read Full Article</a></li>
         
                 </ul>
-                <button onClick={() => handleFavoritesClick(article)}>Add to Favorites</button>
+                <button onClick={() => props.handleFavoritesClick(article)}>Add to Favorites</button>
 
-
-                {/* <p>Date: {article.publishedAt}</p>  */}
-                {/* <li>Author: {article.author}</li> */}
             </div>
         )
     })
     return (
         <>
             {displayNews}
-            <Favorites favorites={favorites} handleClick={removeFromFaves} />
         </>
     )
 }
